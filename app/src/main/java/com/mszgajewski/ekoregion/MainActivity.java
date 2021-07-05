237f40a4bbcadfa46e31ace417e1a1b584109736
Mariusz <56251640+mszgajewski@users.noreply.github.com>
2021-07-05 20:42:40 +0200

    Add files via upload

--- app/src/main/java/com/mszgajewski/ekoregion/MainActivity.java
+++ app/src/main/java/com/mszgajewski/ekoregion/MainActivity.java
@@ -5,45 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView  autoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this, "Połaczenie z firebase udane",
-                Toast.LENGTH_LONG).show();
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+
+        String[] lokalizacja = getResources().getStringArray(R.array.Lokalicacja);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.dropdown_item, lokalizacja);
+        autoCompleteTextView.setText(arrayAdapter.getItem(0).toString(),false);
+
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
-
-    }
-    
-     public boolean onContextItemSelected(MenuItem item) {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        switch (item.getItemId()) {
-            case R.id.lokalizacja:
-                LOCATION_SERVICE(info.id);
-                return true;
-            case R.id.Ustawienia:
-                SEARCH_SERVICE(info.id);
-                return true;
-            default:
-                return super.onContextItemSelected(item);
-        }
-    }
-
-    private void SEARCH_SERVICE(long id) {
-    }
-
-    private void LOCATION_SERVICE(long id) {
     }
 }
