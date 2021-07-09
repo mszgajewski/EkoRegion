f116902de7bfd49ca13082563ce72dbe9ba56b19
Mariusz <56251640+mszgajewski@users.noreply.github.com>
2021-07-09 06:42:20 +0200

    Add files via upload

--- app/src/main/java/com/mszgajewski/ekoregion/MainActivity.java
+++ app/src/main/java/com/mszgajewski/ekoregion/MainActivity.java
@@ -2,27 +2,47 @@ package com.mszgajewski.ekoregion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    AutoCompleteTextView  autoCompleteTextView;
+   private AutoCompleteTextView  autoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
-
         String[] lokalizacja = getResources().getStringArray(R.array.Lokalicacja);
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.dropdown_item, lokalizacja);
-        autoCompleteTextView.setText(arrayAdapter.getItem(0).toString(),false);
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
+        autoCompleteTextView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (parent.getItemAtPosition(position).equals("Lokalizacja")) {
+                } else {
+
+                    String item = parent.getItemAtPosition(position).toString();
+
+                    Toast.makeText(parent.getContext(), "Wybrano: " + item, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
     @Override
@@ -31,4 +51,6 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
+
 }
+
